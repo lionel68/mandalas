b347247b15b66d1fede7690688be3303ad336db0
lhertzog <7189850+lionel68@users.noreply.github.com>
2018-02-15 19:16:24 +0100

    use sf to add colors to the mandalas

--- mandala.R
+++ mandala.R
@@ -2,6 +2,8 @@
 library(ggplot2)
 library(dplyr)
 library(deldir)
+library(sf)
+library(viridis)
 
 # Parameters to change as you like
 iter=3 # Number of iterations (depth)
@@ -29,27 +31,39 @@ for (k in 1:iter)
 # Obtain Voronoi regions
 df %>%
   select(x,y) %>% 
-  deldir(sort=TRUE) %>% 
-  .$dirsgs -> data
+  deldir(sort=TRUE) -> tmp
 
-# Plot regions with geom_segmen
-data %>% 
-  ggplot() +
-  geom_segment(aes(x = x1, y = y1, xend = x2, yend = y2), color="black") +
-  scale_x_continuous(expand=c(0,0))+
-  scale_y_continuous(expand=c(0,0))+
-  coord_fixed() +
-  theme(legend.position  = "none",
-        panel.background = element_rect(fill="white"),
-        panel.border     = element_rect(colour = "black", fill=NA),
-        axis.ticks       = element_blank(),
-        panel.grid       = element_blank(),
-        axis.title       = element_blank(),
-        axis.text        = element_blank())->plot
+#turn into a list of polygons keeping up the area for later use
+tmp <- tile.list(tmp)
+polys <- vector(mode='list',length=length(tmp))
+dat <- NULL
+for(i in seq_along(polys)){
+  mat <- matrix(c(tmp[[i]]$x,tmp[[i]]$x[1],tmp[[i]]$y,tmp[[i]]$y[1]),
+                ncol=2,byrow=FALSE)
+  polys[[i]] <- st_polygon(list(mat))
+  dat <- rbind(dat,data.frame(AREA=tmp[[i]]$area))
+}
 
-plot  
 
-# Do you like the result? Save it (change the name if you want)
-ggsave("mandala.png", height=5, width=5, units='in', dpi=600)
+## create multipolygon sf objects
+pp <- st_sf(dat,st_sfc(polys),row.names=1:length(tmp)) 
+
+#a quick one, color depending on polygon area
+plot(pp,pal=viridis(19,direction = -1,option = "B"),
+     breaks=seq(1,max(pp$AREA),length.out = 20))
+#color depending on row index
+pp$Nb <- 1:nrow(pp)
+plot(pp["Nb"],pal=viridis(19,direction = -1,option = "D"),
+     breaks=seq(1,nrow(pp),length.out = 20))
+
+#choose the colors
+pp <- arrange(pp,AREA) #arranging to find out which polygon belong to which area
+cols <- c("darkgoldenrod3","brown3","yellow2","seashell","salmon","salmon4","seashell3","seashell","red3",
+          "red3","palevioletred3","seashell","seashell2","yellow2","red3","palevioletred3")
+
+png(filename = "Mand1.png",width = 600,height=600,res=200)
+plot(pp["AREA"],pal=cols,
+     breaks=c(0,unique(pp$AREA)),main = "",key.pos=NULL)
+dev.off()
 
 
